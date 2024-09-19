4a0257eee6e390a5b61260cc88d5115b2640e7b1
Markel Uribe Lasa <uribemarkel@gmail.com>
2024-09-19 16:14:30 +0200

    first

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+
+int	main(int argc, char *argv[])
+{
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" << std::endl;
+	else
+	{
+		for (int j=1; j < argc; j++)
+		{
+			for (int i=0; i < (int)strlen(argv[j]); i++)
+				std::cout << (char)toupper(argv[j][i]);
+		}
+		std::cout << "\n";
+	}
+	return 0;
+}
