3de26449ccd5827f88c09b10fcf72831dbde41dc
MarkelUribe <uribemarkel@gmail.com>
2025-01-12 18:11:51 +0100

    cokin fixed

--- module02/ex00/Fixed.hpp
+++ module02/ex00/Fixed.hpp
@@ -6,13 +6,13 @@
 class Fixed
 {
 private:
-	int 				_number;
-	static const int	_fraction;
+	int					_number;
+	static const int	_fraction = 8;
 
 public:
 	Fixed();
 	Fixed(const Fixed &fixed);
-	Fixed operator = (const Fixed &fixed);
+	Fixed &operator= (const Fixed &fixed);
 	~Fixed();
 	int getRawBits( void ) const;
 	void setRawBits( int const raw );
---
+++ module02/ex01/Fixed.hpp
@@ -0,0 +1,27 @@
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	int					_number;
+	static const int	_fraction = 8;
+
+public:
+	Fixed();
+	Fixed(const int number);
+	Fixed(const float number);
+	Fixed(const Fixed &fixed);
+	Fixed &operator= (const Fixed &fixed);
+	~Fixed();
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat(void) const;
+	int		toInt(void) const;
+};
+
+std::ostream& operator<< (std::ostream &out, const Fixed &c);
+
+#endif
