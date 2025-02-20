0c8bd7c29ae7316b9419b32a90a1ab46b58f37ca
Markel Uribe Lasa <uribemarkel@gmail.com>
2025-02-20 16:18:06 +0100

    eyo

--- module01/ex03/HumanA.cpp
+++ module01/ex03/HumanA.cpp
@@ -4,5 +4,5 @@ HumanA::HumanA(std::string name, Weapon weapon) : _name(name), _weapon(weapon) {
 HumanA::~HumanA(void) {};
 
 void HumanA::attack(void) {
-	std::cout << _name << "attacks with their" << _weapon.getType() << std::endl;
+	std::cout << _name << " attacks with their " << _weapon.getType() << std::endl;
 }
--- module01/ex03/HumanB.cpp
+++ module01/ex03/HumanB.cpp
@@ -4,7 +4,7 @@ HumanB::HumanB(std::string name) : _name(name), _weapon(NULL){};
 HumanB::~HumanB(void) {};
 
 void HumanB::attack(void) {
-	std::cout << _name << "attacks with their" << (*_weapon).getType() << std::endl;
+	std::cout << _name << " attacks with their " << (*_weapon).getType() << std::endl;
 }
 
 void HumanB::setWeapon(Weapon& weapon) {
