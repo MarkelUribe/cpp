90437742c70a7083cf6b387d191c00368683d63e
MarkelUribe <uribemarkel@gmail.com>
2025-03-04 19:37:19 +0100

    module05

---
+++ module05/ex00/Bureaucrat.cpp
@@ -0,0 +1,38 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(void) : name("Default"), grade(150) {};
+
+Bureaucrat::Bureaucrat(std::string name, unsigned int grade) : name(name), grade(grade) {
+	if (grade > 150)
+		throw GradeTooHighException();
+	if (grade < 1)
+		throw GradeTooLowException();
+	this->grade = grade;
+};
+
+std::string	Bureaucrat::getName(void) const{
+	return this->name;
+}
+
+unsigned int	Bureaucrat::getGrade(void) const {
+	return this->grade;
+}
+
+void	Bureaucrat::incrementGrade(void) {
+	this->grade++;
+	if (grade > 150)
+		throw GradeTooHighException();
+}
+
+void	Bureaucrat::decrementGrade(void) {
+	this->grade--;
+	if (grade < 1)
+		throw GradeTooLowException();
+}
+
+std::ostream	&operator<<(std::ostream &ostream, const Bureaucrat &model) {
+
+	ostream << model.getName() << ", bureaucrat grade " << model.getGrade();
+	
+	return(ostream);
+}
---
+++ module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <stdexcept>
+
+class Bureaucrat
+{
+private:
+	const std::string	name;
+	unsigned int		grade;
+
+public:
+	Bureaucrat();
+	Bureaucrat(std::string name, unsigned int grade);
+	Bureaucrat(const Bureaucrat &copy);
+	Bureaucrat &operator= (const Bureaucrat &other);
+	~Bureaucrat();
+
+	class	GradeTooHighException: public std::exception {
+		public:
+			const char *what() const throw() {
+				return "Grade too high";
+			}
+	};
+	class	GradeTooLowException: public std::exception {
+		public:
+			const char *what() const throw() {
+				return "Grade too low";
+			}
+	};
+
+	std::string		getName(void) const;
+	unsigned int	getGrade(void) const;
+
+	void		incrementGrade(void);
+	void		decrementGrade(void);
+};
+
+std::ostream	&operator<<(std::ostream &_ostream, const Bureaucrat &_bureaucrat);
+
+#endif
